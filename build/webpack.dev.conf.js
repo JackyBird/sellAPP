aa186cb8eb1cd7eccc44b02a945128db1edb78f3
huanglei <huangl@yrdce.com>
2017-12-17 23:44:02 +0800

    add router

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -50,7 +50,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         res.json({
           errno: 0,
           data: seller
-        })//接口返回json数据，上面配置的数据seller就赋值给data请求后调用
+        })
       }),
       app.get('/api/goods', (req, res) => {
         res.json({
